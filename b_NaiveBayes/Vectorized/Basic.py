78124b56a26ec68efb3c517a4a2420860b6e4a75
wangjiangtao-NJPI <57181890@qq.com>
2018-04-08 09:48:27 +0800

    添加中文注释

--- b_NaiveBayes/Vectorized/Basic.py
+++ b_NaiveBayes/Vectorized/Basic.py
@@ -1,18 +1,32 @@
+# OS模块是Python系统编程的操作模块，提供对操作系统进行调用的接口；
+# sys模块包含了与Python解释器和它的环境有关的函数；
 import os
 import sys
+
+
+# os.path.abspath(path)返回path规范化的绝对路径；
+# 以下代码将工作模块加入到搜索路径中；
 root_path = os.path.abspath("../../")
 if root_path not in sys.path:
     sys.path.append(root_path)
 
+
+# numpy（Numerical Python）提供了python对多维数组对象的支持；
 import numpy as np
+# 调用参数pi；
 from math import pi
-
+# 调用Timing模块；
 from Util.Timing import Timing
+# 调用ClassifierBase模块；
 from Util.Bases import ClassifierBase
-
+# 指数运算符 ** 来计算平方根；
 sqrt_pi = (2 * pi) ** 0.5
 
 
+# http://www.runoob.com/python3/python3-class.html；
+# https://blog.csdn.net/u010157603/article/details/50999108###；
+# classmethod 和普通函数调用时都有默认参数传入，只有staticmethod调用时没有任何默认参数传入；
+# 类NBFunctions用于GaussianNB；
 class NBFunctions:
     @staticmethod
     def gaussian(x, mu, sigma):
@@ -34,10 +48,32 @@ class NBFunctions:
         return [func(_c=c) for c in range(n_category)]
 
 
+# 朴素贝叶斯模型基（父）类，也是ClassifierBase的派生（子）类；
 class NaiveBayes(ClassifierBase):
     NaiveBayesTiming = Timing()
 
+    # 初始化结构
+    # self._x 、 self._y：记录训练集变量；
+    # self._data：核心数组，存储条件概率；
+    # self._n_possibilities：记录各维度特征值个数的数组；
+    # self._p_category：
+    # self._labelled_x ：记录按类别分开后的输入数据的数组；
+    # self._label_zip：记录类别相关信息；
+    # self._cat_counter ：记录第i类数据的个数；
+    # self._con_counter：记录条件概率的原始极大似然估计；
+    # self.label_dict：记录数值化类别时的转换关系；
+    # self._feat_dicts：记录数值化各维度特征时的转换关系；
+    # self._params["lb"] ：
+
     def __init__(self, **kwargs):
+        # **kwargs：表示的就是形参中按照关键字传值把多余的传值以字典的方式呈现；
+        # def foo(x,**kwargs):
+        #     print(x)
+        #     print(kwargs)
+        # foo(1,y=1,a=2,b=3,c=4)#将y=1,a=2,b=3,c=4以字典的方式给了kwargs
+        # 执行结果是：
+        # 1
+        # {'y': 1, 'a': 2, 'b': 3, 'c': 4}
         super(NaiveBayes, self).__init__(**kwargs)
         self._x = self._y = self._data = None
         self._n_possibilities = self._p_category = None
@@ -47,17 +83,21 @@ class NaiveBayes(ClassifierBase):
 
         self._params["lb"] = kwargs.get("lb", 1)
 
+    # sample_weight：样本权重；
     def feed_data(self, x, y, sample_weight=None):
+        # pass 不做任何事情，一般用做占位语句；
         pass
 
     def feed_sample_weight(self, sample_weight=None):
         pass
 
+    # 定义计算先验概率的函数，lb为平滑项，取1时为拉普拉斯平滑；
     @NaiveBayesTiming.timeit(level=2, prefix="[API] ")
     def get_prior_probability(self, lb=1):
         return [(c_num + lb) / (len(self._y) + lb * len(self._cat_counter))
                 for c_num in self._cat_counter]
 
+    # 定义具有普适性的训练函数；
     @NaiveBayesTiming.timeit(level=2, prefix="[API] ")
     def fit(self, x=None, y=None, sample_weight=None, lb=None):
         if sample_weight is None:
@@ -74,18 +114,26 @@ class NaiveBayes(ClassifierBase):
     def _func(self, x, i):
         pass
 
+    # 定义预测单一样本的函数；
     @NaiveBayesTiming.timeit(level=1, prefix="[API] ")
     def predict(self, x, get_raw_result=False, **kwargs):
+        # 在预测前，先把新的输入数据数值化；
+        # 如输入Numpy数组，则转化为Python数组；
+        # 因为Python数组操作更快；
         if isinstance(x, np.ndarray):
             x = x.tolist()
+        # 否则，对数组进行拷贝；
         else:
             x = [xx[:] for xx in x]
+        # 调用相关方法数值化，根据模型的不同而不同；
         x = self._transfer_x(x)
         m_arg, m_probability = np.zeros(len(x), dtype=np.int8), np.zeros(len(x))
+        # 遍历各类别，找到能使后验概率最大化的类别；
         for i in range(len(self._cat_counter)):
             p = self._func(x, i)
             mask = p > m_probability
             m_arg[mask], m_probability[mask] = i, p[mask]
+        # 定义预测多样本的函数；
         if not get_raw_result:
             return np.array([self.label_dict[arg] for arg in m_arg])
         return m_probability
--- b_NaiveBayes/Vectorized/MultinomialNB.py
+++ b_NaiveBayes/Vectorized/MultinomialNB.py
@@ -1,13 +1,20 @@
+# OS模块是Python系统编程的操作模块，提供对操作系统进行调用的接口；
+# sys模块包含了与Python解释器和它的环境有关的函数；
 import os
 import sys
+
+# os.path.abspath(path)返回path规范化的绝对路径；
+# 以下代码将工作模块加入到搜索路径中；
 root_path = os.path.abspath("../../")
 if root_path not in sys.path:
     sys.path.append(root_path)
 
+# matplotlib是python上的一个2D绘图库；
 import matplotlib.pyplot as plt
 
-from b_NaiveBayes.Vectorized.Basic import *
 
+# 调入Basic、DataUtil、Timing模块；
+from b_NaiveBayes.Vectorized.Basic import *
 from Util.Util import DataUtil
 from Util.Timing import Timing
 
@@ -15,26 +22,40 @@ from Util.Timing import Timing
 class MultinomialNB(NaiveBayes):
     MultinomialNBTiming = Timing()
 
+    # 定义预处理数据的方法；
     @MultinomialNBTiming.timeit(level=1, prefix="[API] ")
     def feed_data(self, x, y, sample_weight=None):
+        #
         if sample_weight is not None:
             sample_weight = np.asarray(sample_weight)
         x, y, _, features, feat_dicts, label_dict = DataUtil.quantize_data(x, y, wc=np.array([False] * len(x[0])))
+
+        # 利用Numpy中bincount方法，获得各类别数据的个数；
         cat_counter = np.bincount(y)
+        # 记录各维度特征的取值个数；
         n_possibilities = [len(feats) for feats in features]
 
+        # 获得各类别数据的下标；
         labels = [y == value for value in range(len(cat_counter))]
+
+        # 利用下标获取按类别分开后的输入数据的数组；
         labelled_x = [x[ci].T for ci in labels]
 
+        # 更新模型的各个属性；
         self._x, self._y = x, y
         self._labelled_x, self._label_zip = labelled_x, list(zip(labels, labelled_x))
         self._cat_counter, self._feat_dicts, self._n_possibilities = cat_counter, feat_dicts, n_possibilities
         self.label_dict = label_dict
+
+        # 调用处理样本权重的函数，以更新记录条件概率的数组；
         self.feed_sample_weight(sample_weight)
 
+    # 定义处理样本权重的函数
     @MultinomialNBTiming.timeit(level=1, prefix="[Core] ")
     def feed_sample_weight(self, sample_weight=None):
         self._con_counter = []
+
+        # 利用Numpy中bincount方法，获得带权重的条件概率的极大似然估计；
         for dim, p in enumerate(self._n_possibilities):
             if sample_weight is None:
                 self._con_counter.append([
@@ -44,6 +65,7 @@ class MultinomialNB(NaiveBayes):
                     np.bincount(xx[dim], weights=sample_weight[label] / sample_weight[label].mean(), minlength=p)
                     for label, xx in self._label_zip])
 
+    # 定义核心训练函数；
     @MultinomialNBTiming.timeit(level=1, prefix="[Core] ")
     def _fit(self, lb):
         n_dim = len(self._n_possibilities)
@@ -58,13 +80,17 @@ class MultinomialNB(NaiveBayes):
         self._data = [np.asarray(dim_info) for dim_info in data]
 
     @MultinomialNBTiming.timeit(level=1, prefix="[Core] ")
+    # 生成决策函数；
     def _func(self, x, i):
         x = np.atleast_2d(x).T
         rs = np.ones(x.shape[1])
+        # 遍历各维度，利用data和条件独立性假设计算联合条件概率；
         for d, xx in enumerate(x):
             rs *= self._data[d][i][xx]
+        # 利用先验概率和联合条件概率计算后验概率；
         return rs * self._p_category[i]
 
+    # 定义数值化数据的函数；
     @MultinomialNBTiming.timeit(level=1, prefix="[Core] ")
     def _transfer_x(self, x):
         for i, sample in enumerate(x):
@@ -72,6 +98,7 @@ class MultinomialNB(NaiveBayes):
                 x[i][j] = self._feat_dicts[j][char]
         return x
 
+    # 可视化；
     def visualize(self, save=False):
         colors = plt.cm.Paired([i / len(self.label_dict) for i in range(len(self.label_dict))])
         colors = {cat: color for cat, color in zip(self.label_dict.values(), colors)}
@@ -95,6 +122,7 @@ class MultinomialNB(NaiveBayes):
             else:
                 plt.savefig("d{}".format(j + 1))
 
+# 评估；
 if __name__ == '__main__':
     import time
 
@@ -102,6 +130,7 @@ if __name__ == '__main__':
     (x_train, y_train), (x_test, y_test) = DataUtil.get_dataset(
         "mushroom", "../../_Data/mushroom.txt", n_train=train_num, tar_idx=0)
 
+    # 实例化模型，记录时间；
     learning_time = time.time()
     nb = MultinomialNB()
     nb.fit(x_train, y_train)
@@ -109,7 +138,9 @@ if __name__ == '__main__':
     estimation_time = time.time()
     nb.evaluate(x_train, y_train)
     nb.evaluate(x_test, y_test)
+    # 评估模型表现，记录评估花费的时间；
     estimation_time = time.time() - estimation_time
+    # 将记录下来的耗时输出；
     print(
         "Model building  : {:12.6} s\n"
         "Estimation      : {:12.6} s\n"
